0d941abf6567cb2149aba1acbb6e8567994fff37
Paul R. Carlson <carlsonpar@gmail.com>
2020-12-21 17:53:55 -0500

    code to parse out the markdown file and create an html file

--- src/posts.js
+++ src/posts.js
@@ -15,9 +15,28 @@ const postHTML = data => `
 </html>`
 
 const createPost = postPath => {
-    const data = fs.readFileSync(`${config.dev.postdir}/${postPath}`,"utf8")
+    const data = fs.readFileSync(`${config.dev}/${postPath}`,"utf8")
     const content = fm(data)
     content.body  = marked(content.body)
     content.path = postPath;
     return content
+}
+
+const createPosts = posts => {
+    posts.forEach(post=>{
+        if(!fs.existsSync(`${config.dev.outdir}/${post.path}`))
+            fs.mkdirSync(`${config.dev.outdir}/${post.path}`)
+        fs.writeFile(
+            `${config.dev.outdir}/${post.path}/index.html`,
+            posthtml(post),
+            e => {
+                if (e) throw e
+                console.log('success!')
+            }
+        )
+    })
+}
+module.exports = {
+    createPost: createPost,
+    createPosts: createPosts
 }
