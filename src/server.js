75b4009818e2a434e92105fedf2aee1d94c2eae4
Paul R. Carlson <carlsonpar@gmail.com>
2020-12-23 16:03:01 -0500

    fixed issue so now the links take you to the correct page

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log(req.url)
   
-  fs.readFile("./index.html")
+  fs.readFile(`./public/${req.url}`)
   .then(contents =>{
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
@@ -18,5 +19,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  open(`http://${hostname}:${port}/`)
+  // open(`http://${hostname}:${port}/`)
 });
