8301e5eb80d27e7fdcb6977dfad37417ad8e95ee
Paul R. Carlson <carlsonpar@gmail.com>
2020-12-23 16:34:48 -0500

    added the running of the build script each time a change acures on eithe r a file or the content directory

--- src/services/observer.js
+++ src/services/observer.js
@@ -1,7 +1,19 @@
 const chokidar = require('chokidar')
 const EventEmitter = require('events').EventEmitter
 const fse = require('fs-extra')
+const postMethods = require('../posts')
+const addHomePage = require('../main.js')
+const config = require('../config')
 
+const create = () =>{
+    const posts = fse
+                    .readdirSync(config.dev.postdir)
+                    .map(post => post.replace('.md',''))
+                    .map(post => postMethods.createPost(post))
+
+                postMethods.createPosts(posts)
+                addHomePage(posts)
+}
 class Observer extends EventEmitter {
     constructor(){
         super()
@@ -11,8 +23,9 @@ class Observer extends EventEmitter {
         console.log(`Watching for changes on: ${folder}`)
         var watcher = chokidar.watch(folder, {persistent: true})
         watcher
-            .on('add', filePath=>{console.log('add')})
-            .on('unlink', filePath=>{console.log('gone')})
+            .on('add', filePath=>{create()})
+            .on('change', filePath=>{create()})
+            .on('unlink', filePath=>{create()})
         
 
     } catch(error) {
